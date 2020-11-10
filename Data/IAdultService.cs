c16ce5a34d7c4235550755cbf0f7611eee3abf6f
audrius145 <58250824+audrius145@users.noreply.github.com>
2020-11-10 20:20:18 +0100

    Assignment2

---
+++ Data/IAdultService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+
+namespace WebApplication7.Data
+{
+    public interface IAdultService
+    {
+        Task<IList<Adult>> getAdultsAsync();
+        Task AddAdultAsync(Adult adult);
+        Task RemoveAdultAsync(int adultID);
+        
+    }
+}
